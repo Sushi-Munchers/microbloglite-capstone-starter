e04b5fb311fe9db0a63ef2e186faefa35c8c29c4
Toria Sellers <victoria.12292001@gmail.com>
2024-01-04 10:47:21 -0500

    handled merge

--- posts/posts.js
+++ posts/posts.js
@@ -1,41 +1,23 @@
 "use strict";
 
-<<<<<<< HEAD
 // Function to get all users via fetch()
-function getAllUsers () {
-    // GET /api/users
-    const loginData = getLoginData();
-    const options = {
+function getAllUsers() {
+  // GET /api/users
+  const loginData = getLoginData();
+  const options = {
     method: "GET",
     headers: {
-    Authorization: `Bearer ${loginData.token}`,
+      Authorization: `Bearer ${loginData.token}`,
     },
-    };
-    // note: the api variable is defined in auth.js
-    fetch(api + "/api/posts", options)
-    .then(response => response.json());
-    .then(users => {
-    // Do something with the users array...
-    console.log(users);
+  };
+  // note: the api variable is defined in auth.js
+  fetch(api + "/api/posts", options)
+    .then((response) => response.json())
+    .then((users) => {
+      // Do something with the users array...
+      console.log(users);
     });
-    }
-=======
-// const api = "https://microbloglite.herokuapp.com/";
-const $q = (selector) => document.querySelector(selector);
-const fullName = $q("#fullName");
-const username = $q("#username");
-const bio = $q("#bio");
-const editButton = $q("#editButton");
-const postText = $q("#postText");
-const contentDiv = $q("#contentDiv");
-const messagePara = $q("#messagePara");
-const editForm = $q("#editForm");
-editForm.style.display = "none";
-// const passwordInput = $q("#passwordInput");
-// const fullNameInput = $q("#fullNameInput");
-const bioInput = $q("#bioInput");
-const messageDiv = $q("#messageDiv");
->>>>>>> f0366b6f21b1c94a9ce285de4e9c13a669375cbd
+}
 
 // You can use this to get the login data of the logged-in user (if any).
 // Returns either an object including the username and token,
@@ -57,7 +39,10 @@ function loadProfileInfo() {
       "Content-Type": "application/json",
     },
   };
-  fetch("https://microbloglite.herokuapp.com/" + "api/users/" + loginData.username, options)
+  fetch(
+    "https://microbloglite.herokuapp.com/" + "api/users/" + loginData.username,
+    options
+  )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -102,61 +87,17 @@ function hideEditForm() {
 
 function editProfile(event) {
   event.preventDefault();
-    const loginData = getLoginData();
-
-    const options = {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${loginData.token}`,
-        },
-<<<<<<< HEAD
-    };
-
-    fetch(apiBaseURL + "/auth/logout", options)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .finally(() => {
-            // We're using `finally()` so that we will continue with the
-            // browser side of logging out (below) even if there is an 
-            // error with the fetch request above.
-
-            window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
-            window.location.assign("/");  // redirect back to landing page
-        });
-}
-=======
-        body: JSON.stringify({
-          // password: passwordInput.value,
-          bio: bioInput.value,
-          // fullName: fullNameInput.value,
-        })
-    }
-    fetch("https://microbloglite.herokuapp.com/" + "api/users/" + loginData.username, options)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      messageDiv.innerText = `Saved successfully! Please refresh your room.`
-      // window.location.replace("/profilePage/");
-    });
-}
-
-function logout() {
   const loginData = getLoginData();
 
-  // GET /auth/logout
   const options = {
-    method: "GET",
+    method: "PUT",
     headers: {
-      // This header is how we authenticate our user with the
-      // server for any API requests which require the user
-      // to be logged-in in order to have access.
-      // In the API docs, these endpoints display a lock icon.
+      "Content-Type": "application/json",
       Authorization: `Bearer ${loginData.token}`,
     },
   };
 
-  fetch("https://microbloglite.herokuapp.com/" + "/auth/logout", options)
+  fetch(apiBaseURL + "/auth/logout", options)
     .then((response) => response.json())
     .then((data) => console.log(data))
     .finally(() => {
@@ -165,12 +106,6 @@ function logout() {
       // error with the fetch request above.
 
       window.localStorage.removeItem("login-data"); // remove login data from LocalStorage
-      window.location.assign("/logInPage/"); // redirect to landing page
+      window.location.assign("/"); // redirect back to landing page
     });
 }
-
-window.onload = () => {
-  editButton.onlick = editProfile;
-  loadProfileInfo();
-};
->>>>>>> f0366b6f21b1c94a9ce285de4e9c13a669375cbd
